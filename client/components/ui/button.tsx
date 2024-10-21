428a98a08d80b4ea47c50bd0d3f316e93b1e065f
Roberto Cedeno <150482419+robced@users.noreply.github.com>
2024-10-21 11:57:42 -0700

    style -> WIP button variant
    Partially finished adding the variants for the button component

--- client/components/ui/button.tsx
+++ client/components/ui/button.tsx
@@ -4,29 +4,56 @@ import { twMerge } from "tailwind-merge";
 import { cva, VariantProps } from "class-variance-authority";
 import React from "react";
 
-const buttonStyles = cva(
-    "px-4 py-2 rounded-md", // Base styles
+const buttonVariants = cva(
+    "rounded-full font-bold text-base flex items-center justify-center", // Base styles
     {
         variants: {
-            intent: {
-                primary: "bg-blue-500 text-white",
-                secondary: "bg-gray-500 text-white",
-                danger: "bg-red-500 text-white",
+            variant: {
+                primary: "bg-primary",
+                secondary: "bg-secondary",
+                destructive: "bg-destructive",
+                outline: "border-2 border-primary",
+                ghost: "bg-primary/10",
+                link: "",
+                icon: "",
+                withIcon: "",
             },
             size: {
-                small: "text-sm",
-                medium: "text-base",
-                large: "text-lg",
+                small: "h-8 px-4",
+                medium: "h-10 px-6",
+                large: "h-12 px-8",
+                fullWidth: "h-12 self-stretch"
             },
         },
         defaultVariants: {
-            intent: "primary",
+            variant: "primary",
             size: "medium",
         },
     }
 );
 
-interface ButtonProps extends VariantProps<typeof buttonStyles> {
+const buttonTextVariants = cva(
+    "text-base font-bold",
+    {
+        variants: {
+            variant: {
+                primary: "text-white",
+                secondary: "text-white",
+                destructive: "text-white",
+                outline: "text-primary",
+                ghost: "text-primary",
+                link: "underline",
+                icon: "",
+                withIcon: "",
+            }
+        },
+        defaultVariants: {
+            variant: "primary"
+        }
+    }
+)
+
+interface ButtonProps extends VariantProps<typeof buttonVariants> {
     children: React.ReactNode;
     onPress?: () => void;
     className?: string;
@@ -34,16 +61,18 @@ interface ButtonProps extends VariantProps<typeof buttonStyles> {
 
 const Button: React.FC<ButtonProps> = ({
                                            children,
-                                           intent,
+                                           variant,
                                            size,
                                            onPress,
                                            className,
                                        }) => {
-    const mergedClassName = twMerge(clsx(buttonStyles({ intent, size }), className));
+    const mergedClassName = twMerge(clsx(buttonVariants({ variant, size }), className));
+    const mergedTextClassName = twMerge(clsx(buttonTextVariants({ variant })));
+
 
     return (
         <Pressable onPress={onPress} className={mergedClassName}>
-            <Text>{children}</Text>
+            <Text className={mergedTextClassName}>{children}</Text>
         </Pressable>
     );
 };
