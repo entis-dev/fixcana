0c2ac9710187c85f24710c1ece8bf1afef5ab6e3
Roberto Cedeno <150482419+robced@users.noreply.github.com>
2024-10-22 12:30:52 -0700

    style -> add PlusJakartasans

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -3,10 +3,11 @@ import "../styles/global.css"
 import {NAV_THEME} from "@/lib/constants";
 import {Theme, ThemeProvider} from "@react-navigation/native";
 import {useColorScheme} from "@/lib/useColorScheme";
-import React from "react";
+import React, {useEffect} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {Platform} from "react-native";
 import { StatusBar } from 'expo-status-bar';
+import {useFonts} from "expo-font";
 
 const LIGHT_THEME: Theme = {
     dark: false,
@@ -23,8 +24,28 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
     const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
     const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+    const [loaded] = useFonts({
+        "PlusJakartaSans-Bold": require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+        "PlusJakartaSans-BoldItalic": require('../assets/fonts/PlusJakartaSans-BoldItalic.ttf'),
+        "PlusJakartaSans-ExtraBold": require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+        "PlusJakartaSans-ExtraBoldItalic": require('../assets/fonts/PlusJakartaSans-ExtraBoldItalic.ttf'),
+        "PlusJakartaSans-ExtraLight": require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
+        "PlusJakartaSans-ExtraLightItalic": require('../assets/fonts/PlusJakartaSans-ExtraLightItalic.ttf'),
+        "PlusJakartaSans-Italic": require('../assets/fonts/PlusJakartaSans-Italic.ttf'),
+        "PlusJakartaSans-Light": require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+        "PlusJakartaSans-LightItalic": require('../assets/fonts/PlusJakartaSans-LightItalic.ttf'),
+        "PlusJakartaSans-Medium": require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+        "PlusJakartaSans-MediumItalic": require('../assets/fonts/PlusJakartaSans-MediumItalic.ttf'),
+        "PlusJakartaSans-Regular": require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+        "PlusJakartaSans-SemiBold": require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+        "PlusJakartaSans-SemiBoldItalic": require('../assets/fonts/PlusJakartaSans-SemiBoldItalic.ttf'),
+    })
 
-    React.useEffect(() => {
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
         (async () => {
             const theme = await AsyncStorage.getItem('theme');
             if (Platform.OS === 'web') {
