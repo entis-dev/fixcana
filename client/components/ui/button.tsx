0e13f96cd6d89f2e37c559a939de14b22fb94bac
Roberto Cedeno <150482419+robced@users.noreply.github.com>
2024-10-22 12:37:50 -0700

    style --> WIP button variants

--- client/components/ui/button.tsx
+++ client/components/ui/button.tsx
@@ -1,11 +1,13 @@
-import { Pressable, Text } from "react-native";
+import {Pressable, Text, View} from "react-native";
 import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-import { cva, VariantProps } from "class-variance-authority";
+import {twMerge} from "tailwind-merge";
+import {cva, VariantProps} from "class-variance-authority";
+import Icon from '@expo/vector-icons/FontAwesome6';
 import React from "react";
+import {cn} from "@/lib/utils";
 
 const buttonVariants = cva(
-    "rounded-full font-bold text-base flex items-center justify-center", // Base styles
+    "rounded-full flex items-center justify-center flex-row gap-2", // Base styles
     {
         variants: {
             variant: {
@@ -15,10 +17,9 @@ const buttonVariants = cva(
                 outline: "border-2 border-primary",
                 ghost: "bg-primary/10",
                 link: "",
-                icon: "",
-                withIcon: "",
             },
             size: {
+                icon: "w-12 h-12",
                 small: "h-8 px-4",
                 medium: "h-10 px-6",
                 large: "h-12 px-8",
@@ -32,8 +33,26 @@ const buttonVariants = cva(
     }
 );
 
+const buttonIconContainer = cva(
+    "",
+    {
+        variants: {
+            size: {
+                icon: "",
+                small: "",
+                medium: "",
+                large: "",
+                fullWidth: ""
+            },
+        },
+        defaultVariants: {
+            size: "medium",
+        },
+    }
+)
+
 const buttonTextVariants = cva(
-    "text-base font-bold",
+    "text-base font-plusjakarta-bold",
     {
         variants: {
             variant: {
@@ -42,9 +61,7 @@ const buttonTextVariants = cva(
                 destructive: "text-white",
                 outline: "text-primary",
                 ghost: "text-primary",
-                link: "underline",
-                icon: "",
-                withIcon: "",
+                link: "underline text-foreground",
             }
         },
         defaultVariants: {
@@ -56,23 +73,35 @@ const buttonTextVariants = cva(
 interface ButtonProps extends VariantProps<typeof buttonVariants> {
     children: React.ReactNode;
     onPress?: () => void;
+    icon?: React.ReactNode;
     className?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
                                            children,
                                            variant,
+                                           icon,
                                            size,
                                            onPress,
                                            className,
                                        }) => {
-    const mergedClassName = twMerge(clsx(buttonVariants({ variant, size }), className));
-    const mergedTextClassName = twMerge(clsx(buttonTextVariants({ variant })));
-
+    const mergedClassName = twMerge(clsx(buttonVariants({variant, size}), className));
+    const mergedTextClassName = twMerge(clsx(buttonTextVariants({variant})));
+    const mergedIconContainerClassName = twMerge(clsx(buttonIconContainer({size})));
 
     return (
-        <Pressable onPress={onPress} className={mergedClassName}>
+        <Pressable onPress={onPress} className={
+            cn(
+                mergedClassName,
+                (icon && size === "fullWidth") && "justify-between px-4"
+            )}>
+            {icon ? <View className={mergedIconContainerClassName}>
+                <Text className={mergedTextClassName}>{icon}</Text>
+            </View> : null}
             <Text className={mergedTextClassName}>{children}</Text>
+            {(icon && size === "fullWidth") &&
+                <View/>
+            }
         </Pressable>
     );
 };
